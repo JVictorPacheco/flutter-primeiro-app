d1de7c248ba2c3299bfe25560552096eb12752d3
JVictorPacheco <95194054+JVictorPacheco@users.noreply.github.com>
2022-07-22 16:09:11 -0300

    Add files via upload
    Alterando main.dart para teste

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ByteBank());
+  //save(Contact(0, 'vitinho', 1983));
+}
+
+class ByteBank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Colors.green[900],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          ),
+          appBarTheme: AppBarTheme(color: Colors.green[900])),
+      home: Dashboard(), //ContactForm(), //Dashboard(),
+    );
+  }
+}
